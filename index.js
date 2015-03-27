22b1dc25b7b122733024cc9adbf4de07e9a88a28
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-27 18:26:12 -0500

    adding bin to package.json, defining some flags

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+
+var program = require('commander'),
+	path = require('path'),
+	pkg = require(path.join(__dirname, 'package.json'));
+
+program
+	.version(pkg.version)
+	.option('-t, --type <type>', 'Type of car you\'d like to request. Options: x, taxi, select, black')
+	.option('-s, --size <size>', 'Size of car you\'d like to request. Options: x or xl')
+	.option('-d, --destination <dest>', 'Destination address. Make sure it\'s enclosed by ""!')
+	.parse(process.argv);
+
+console.log(program.type);
+console.log(program.size);
+console.log(program.destination);
