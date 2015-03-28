6deb8a82d3f005b08adf4c93b9d1ad6f68a77759
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-27 22:46:03 -0500

    file writing and reading working

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var program = require('commander'),
 	path = require('path'),
 	pkg = require(path.join(__dirname, 'package.json')),
 	chalk = require('chalk'),
-	inquirer = require('inquirer');
+	inquirer = require('inquirer'),
+	fs = require('fs');
 
 // variables
 var emphasize = chalk.green.bgBlue;
@@ -34,6 +35,27 @@ var questions = [
 	}
 ];
 
+// functions
+
+function doesFileExist() {
+	return fs.existsSync('.uberconfig.json');
+}
+
+function readFromFile() {
+	fs.readFile('.uberconfig.json', {encoding: 'utf8'}, function(err, data) {
+		var contents = JSON.parse(data);
+		console.log(contents);
+	});
+}
+
+function writeToFile(answers) {
+	fs.writeFile('.uberconfig.json', JSON.stringify(answers), function(err) {
+		if (err) {
+			throw err;
+		}
+	});
+}
+
 program
 	.version(pkg.version)
 	.option('-t, --type <type>', 'Type of car you\'d like to request. ' + emphasize('Options: x, taxi, select, black'))
@@ -41,10 +63,15 @@ program
 	.option('-d, --destination <dest>', 'Destination address. ' + emphasize('Make sure it\'s enclosed by ""!'))
 	.parse(process.argv);
 
-inquirer.prompt(authList, function(answer) {
-	if (answer.authMethod === 'Email') {
-		inquirer.prompt(questions, function(answers) {
-			console.log(answers);
-		})
-	}
-});
+if (!doesFileExist()) {
+	inquirer.prompt(authList, function(answer) {
+		if (answer.authMethod === 'Email') {
+			inquirer.prompt(questions, function(answers) {
+				console.log(answers);
+				writeToFile(answers);
+			});
+		}
+	});
+} else {
+	readFromFile();
+}
